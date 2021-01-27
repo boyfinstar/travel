4d3264071d73f4c1cd089f08d6bc483f24a546a5
Olamide Falowo <33402108+boyfinstar@users.noreply.github.com>
2021-01-27 16:52:48 +0100

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nature</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+</head>
+<body>
+    <h1> hello world </h1>
+</body>
+</html>
