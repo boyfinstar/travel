33a667cc727d1f1f92d4b5318ba743ff895df282
Olamide Falowo <33402108+boyfinstar@users.noreply.github.com>
2021-03-19 08:54:17 +0100

    auth except
    just added portfolio to auth except

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except(['index', 'show']);
+        $this->middleware('auth')->except(['index', 'show', 'services', 'portfolio', 'about']);
     }
 
     /**
